8c919b812c5f48fffcb7e56c58536b1bad7f246e
sofyaperederina <sonya.perederina@mail.ru>
2024-09-16 14:40:26 +0600

    Лабораторная работа за 16.09
    Дана последовательность элементов в количестве n.
    1. Определить количество элементов, меньших 0.
    2. Определить среди элементов второй максимальный элемент.
    3. Определить количество элементов, являющихся локальными минимумами.(элементы, знасения которых меньше соседей: соседа слева и соседа справа)

---
+++ задание 3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int firstElement = Convert.ToInt32(Console.ReadLine());
+            int secondElement = Convert.ToInt32(Console.ReadLine());
+            
+            int ans = 0;
+            for (int i = 0; i < n - 2; i++)
+            {
+                int thirdElement = Convert.ToInt32(Console.ReadLine());
+                if (firstElement>secondElement)
+                {
+                    if (secondElement < thirdElement)
+                    {
+                        ans++;
+                    }
+                }
+                firstElement = secondElement;
+                secondElement = thirdElement;
+            }
+            Console.WriteLine(ans);
+
+        }
+    }
+}
