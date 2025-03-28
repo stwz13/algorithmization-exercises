501f167feaca73ff23d22e27bd26e485282ae63d
sofyaperederina <sonya.perederina@mail.ru>
2025-03-28 19:52:55 +0600

    лабораторная 28.03

---
+++ алгоритмизация/2 семестр/lab 28.03 task2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    //Задача о гонщиках
+    class lab
+    {
+        delegate void Message();
+        class Rider
+        {
+            public string Name { get; set; }
+            public double Speed { get; set; }
+            public double CurrLocation { get; set; }
+            public Rider(double start)
+            {
+                Console.WriteLine("Введите имя и начальную скорость гонщика");
+                string name = Console.ReadLine();
+                double speed = Convert.ToDouble(Console.ReadLine());
+                Name = name;
+                Speed = speed;
+                CurrLocation = start;
+            }
+            public void ChangeLocation(double time)
+            {
+                CurrLocation += time * Speed;
+            }
+            public bool IsFinish(double finish)
+            {
+                WinnerEvent winnerEvent = new WinnerEvent();
+                winnerEvent.WinnerMessage += Win;
+                bool result = CurrLocation >= finish;
+                if (result) winnerEvent.Event();
+                return result;
+            }
+            public void Win()
+            {
+                Console.WriteLine($"{Name} победил");
+            }
+        }
+        class WinnerEvent
+        {
+            public event Message WinnerMessage;
+            public void Event()
+            {
+                if (WinnerMessage != null) WinnerMessage();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты старта и финиша");
+            double start = double.Parse(Console.ReadLine());
+            double finish = double.Parse(Console.ReadLine());
+
+            Rider rider1 = new Rider(start);
+            Rider rider2 = new Rider(start);
+            Rider rider3 = new Rider(start);
+
+            while (true)
+            {
+                Console.WriteLine("Введите время, через которое изменится начальная скорость участников");
+                double time = int.Parse(Console.ReadLine());
+
+                Random newRandom = new Random();
+
+                rider1.ChangeLocation(time);
+                rider2.ChangeLocation(time);
+                rider3.ChangeLocation(time);
+
+                rider1.Speed = newRandom.NextDouble();
+                rider2.Speed = newRandom.NextDouble();
+                rider3.Speed = newRandom.NextDouble();
+
+                if (rider1.IsFinish(finish) || rider2.IsFinish(finish) || rider3.IsFinish(finish)) break;
+
+            }
+            
+        }
+    }
+}
---
+++ алгоритмизация/2 семестр/lab28.03 task1.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+
+
+namespace ConsoleApp3
+{
+       
+    class Task
+    {
+        delegate void PointUser();
+        class Point
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Point()
+            {
+                Console.WriteLine("Введите координаты точки");
+                int x0 = int.Parse(Console.ReadLine());
+                int y0 = int.Parse(Console.ReadLine());
+                x = x0;
+                y = y0;
+            }
+            public void Displacement(int delta_x, int delta_y)
+            {
+                x += delta_x;
+                y += delta_y;
+            }
+        }
+        class Rectangle
+        {
+            public Point point1 { get; set; }
+            public Point point2 { get; set; }
+            public Rectangle(Point point1, Point point2)
+            {
+                this.point1 = point1;
+                this.point2 = point2;
+            }
+            public bool ExtarnalPoint(Point A0)
+            {
+                bool result = (A0.x - point1.x) * (A0.x - point2.x) <= 0 && (A0.y - point1.y) * (A0.y - point2.y) <= 0;
+                if (!result)
+                {
+                    PointEvent currEvent = new PointEvent();
+                    currEvent.pointMessage += WrongPointMessage;
+                    currEvent.OnPointMessage();
+                    return false;
+                }
+                else return true;
+            }
+        }
+        class PointEvent
+        {
+            public event PointUser pointMessage;
+            public void OnPointMessage()
+            {
+                if (pointMessage != null) pointMessage();
+            }
+        }
+        static void WrongPointMessage() => Console.WriteLine("Точка вне прямоугольника");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точек прямоугольника");
+
+            Point pointRectangle1 = new Point();
+            Point pointRectangle2 = new Point();
+            Rectangle myRectangle = new Rectangle(pointRectangle1, pointRectangle2);
+
+            Point myPoint = new Point();
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Введите максимальное абсолютное значение для приращения");
+
+            int max_delta = int.Parse(Console.ReadLine());
+            int delta_x = rnd.Next(-max_delta,max_delta);
+            int delta_y = rnd.Next(-max_delta, max_delta);
+
+
+            myPoint.Displacement(delta_x, delta_y);
+            myRectangle.ExtarnalPoint(myPoint);
+
+        }
+    }
+}
