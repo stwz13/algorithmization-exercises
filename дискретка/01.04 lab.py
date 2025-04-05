d726a01a834a7d54d5e8ce4df6d59b842fbb9733
sofyaperederina <sonya.perederina@mail.ru>
2025-04-05 13:56:49 +0600

    wave alg

--- дискретка/01.04 lab.py
+++ дискретка/01.04 lab.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def init_matrix():
     n = int(input("Введите число строк матрицы"))
     m = int(input("Введите число столбцов матрицы"))
@@ -10,23 +12,24 @@ def init_matrix():
         matrix.append(curr_str)
     matrix.append((m+1)*[-1])
     return matrix
-def walve(matrix, i, j, d):
-    curr_vertexes = [(i, j-1), (i, j+1), (i-1, j), (i+1, j)]
-    change = False
-    for x, y in curr_vertexes:
-        if matrix[x][y] == 0:
-            matrix[x][y] = d + 1
-            change = True
-    return change, d+1
-def min_way(matrix, i_start, j_start, i_finish, j_finish):
-    d = 0
-    d = walve(matrix, i_start, j_start, d)[1]
-    while True:
-        for i in range(1, len(matrix) - 1):
-            for j in range(1, len(matrix[i]) - 1):
-                if matrix[i][j] not in [0, -1]:
-                    result, d = walve(matrix, i, j, d)
-                    if matrix[i_finish][j_finish] != 0:
-                        return matrix[i_finish][j_finish]
-                    else:
-                        if not result: return -1
+def wave_algorithm(matrix, start, finish):
+    i_start, j_start = start
+    i_finish, j_finish = finish
+    if (matrix[i_start][j_start] == -1 or matrix[i_finish][j_finish] == -1):
+        return -1
+
+    next_vertexes = deque()
+    next_vertexes.append((i_start,j_start))
+    while len(next_vertexes)!=0:
+        i,j = next_vertexes.popleft()
+        d = matrix[i][j]
+        if (i,j) == (i_finish,j_finish):
+            return matrix[i_finish][j_finish]
+        elif matrix[i][j] == -1:
+            continue
+        else:
+            for curr_i, curr_j in [(i+1,j), (i-1,j), (i,j-1), (i,j+1)]:
+                if matrix[curr_i][curr_j] == 0 and (curr_i, curr_j) != (i_start, j_start):
+                    matrix[curr_i][curr_j] = d + 1
+                next_vertexes.append((curr_i, curr_j))
+    return -1
